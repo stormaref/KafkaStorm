8bc9c647bec3e570b96f7f508c4cb09e35b0926b
“Aref <storm.aref@gmail.com>
2024-04-17 18:54:53 +0330

    debug closing exception

--- KafkaStorm/Services/ConsumerHostedService.cs
+++ KafkaStorm/Services/ConsumerHostedService.cs
@@ -49,6 +49,7 @@ public class ConsumerHostedService<TMessage> : IHostedService, IDisposable where
 
     private void Handle(CancellationToken cancellationToken)
     {
+        if (cancellationToken.IsCancellationRequested) return;
         var result = _consumer.Consume(1);
         var message = JsonSerializer.Deserialize<TMessage>(result.Message.Value) ??
                       throw new MessageNullException<TMessage>();
